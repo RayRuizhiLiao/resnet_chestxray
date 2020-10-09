83391836886b3e6d39a76eb19e97e0c2ad0a37c5
Ray <ray@Rays-MacBook-Pro-2.local>
2020-10-09 17:16:33 -0400

    modified training code

--- main_utils.py
+++ main_utils.py
@@ -62,18 +62,18 @@ def split_tr_eval(split_list_path, training_folds, evaluation_folds):
 				if int(row[-1]) in training_folds:
 					train_labels[row[2]] = [float(row[3])]
 					train_ids[row[2]] = row[1]
-				if int(row[-1]) in validation_folds and not use_test_data:
+				if int(row[-1]) in evaluation_folds and not use_test_data:
 					val_labels[row[2]] = [float(row[3])]
 					val_ids[row[2]] = row[1]
 			if row[-1] == 'TEST' and use_test_data:
 					test_labels[row[2]] = [float(row[3])]
 					test_ids[row[2]] = row[1]               
 
-	print("Training and validation folds: ", training_folds, validation_folds)
+	print("Training and evaluation folds: ", training_folds, evaluation_folds)
 	print("Total number of training labels: ", len(train_labels))
 	print("Total number of training DICOM IDs: ", len(train_ids))
-	print("Total number of validation labels: ", len(val_labels))
-	print("Total number of validation DICOM IDs: ", len(val_ids))
+	print("Total number of evaluation labels: ", len(val_labels))
+	print("Total number of valuation DICOM IDs: ", len(val_ids))
 	print("Total number of test labels: ", len(test_labels))
 	print("Total number of test DICOM IDs: ", len(test_ids))
 
