1f54a609587fa6804ebf2ffb69cab5876e095f9d
Ruizhi Liao <ruizhi@csail.mit.edu>
2021-01-09 17:35:54 -0500

    updated downsample

--- image_prep/downsample.py
+++ image_prep/downsample.py
@@ -20,7 +20,7 @@ class MimicCxrDataset(torchvision.datasets.VisionDataset):
                     'mimic-cxr-sub-img-edema-split-manualtest.csv'
 
     def __init__(self, transform=None, target_transform=None):
-        super(MimicCxrJpgAllDataset, self).__init__(
+        super(MimicCxrDataset, self).__init__(
             root=None, transform=transform, target_transform=target_transform)
 
         meta_df = pd.read_csv(self.meta_csv_path)
@@ -49,50 +49,52 @@ class MimicCxrDataset(torchvision.datasets.VisionDataset):
         return img, dicom_id, dcm_exists
 
 
-def create_cached_images(im_size, data_folder):
-    dataset = MimicCxrJpgAllDataset(
+def create_cached_images(img_size, save_folder):
+    dataset = MimicCxrDataset(
         transform=torchvision.transforms.Compose([
             # unnormalized uint16 -> int32 (PIL accepts in32, not uint16)
-            torchvision.transforms.Lambda(lambda im: im.astype(np.int32)),
+            torchvision.transforms.Lambda(lambda img: img.astype(np.int32)),
             torchvision.transforms.ToPILImage(),
-            torchvision.transforms.Resize(im_size),
-            torchvision.transforms.CenterCrop(im_size),
+            torchvision.transforms.Resize(img_size),
+            torchvision.transforms.CenterCrop(img_size),
             torchvision.transforms.Lambda(
-                lambda im: np.array(im).astype(np.int32))
+                lambda img: np.array(img).astype(np.int32))
         ]))
     loader = DataLoader(dataset, batch_size=75, shuffle=False,
                         num_workers=15, pin_memory=False)
 
-    ims, dicom_ids_on_disk = [], []
-    for it, (im, dicom_id, dcm_exists) in enumerate(loader):
+    imgs, dicom_ids_on_disk = [], []
+    for it, (img, dicom_id, dcm_exists) in enumerate(loader):
         exists_id = torch.nonzero(dcm_exists)
         dicom_ids_on_disk.append([dicom_id[i] for i in exists_id])
-        ims.append(im[exists_id].squeeze().numpy().astype(np.uint16))
+        imgs.append(img[exists_id].squeeze().numpy().astype(np.uint16))
 
     dicom_ids_on_disk = list(itertools.chain.from_iterable(dicom_ids_on_disk))
-    ims = np.concatenate(ims, axis=0)
-    print(f'Cached a total of {ims.shape[0]} Images')
+    imgs = np.concatenate(imgs, axis=0)
+    print(f'Cached a total of {imgs.shape[0]} Images')
 
-    # Saving
+    '''
+    Save downsampled images 
+    '''
 
-    os.makedirs(data_folder, exist_ok=True)
+    os.makedirs(save_folder, exist_ok=True)
 
-    df = pd.DataFrame({
-        'dicom_id': dicom_ids_on_disk,
-        'index_to_cached_array': list(range(len(dicom_ids_on_disk)))})
-    df_path = os.path.join(
-        data_folder, 'dicom_id_to_cached_array_index_mapping.csv')
+    df = pd.DataFrame({'dicom_id': dicom_ids_on_disk,
+                       'index_to_cached_array': list(range(len(dicom_ids_on_disk)))})
+    df_path = os.path.join(save_folder, 
+                          'dicom_id_to_cached_array_index_mapping.csv')
     df.to_csv(df_path, index=False)
 
-    cache_name = os.path.join(
-        data_folder, f"mimiccxr-{im_size}")
-    np.save(f'{cache_name}.npy', ims)
+    cache_name = os.path.join(save_folder, f"mimiccxr-{img_size}")
+    np.save(f'{cache_name}.npy', imgs)
+
+    return imgs, df
 
 
-for im_size in [256]:
-    start = time.time()
-    create_cached_images(
-        im_size,
-        '/data/vision/polina/scratch/wpq/github/interpretability/notebooks/data/MimicCxrChexpertDataset')
-    end = time.time()
-    print(f'im_size: {im_size} done!')
+# for im_size in [256]:
+#     start = time.time()
+#     create_cached_images(
+#         im_size,
+#         '/data/vision/polina/scratch/wpq/github/interpretability/notebooks/data/MimicCxrChexpertDataset')
+#     end = time.time()
+#     print(f'im_size: {im_size} done!')
