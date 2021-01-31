d2d3abe4e97b8646ba485c8e069c413989ed6bce
Ruizhi Liao <ruizhi@csail.mit.edu>
2021-01-30 19:00:00 -0500

    added code to save 256 png

--- image_prep/data_utils.py
+++ image_prep/data_utils.py
@@ -3,18 +3,31 @@ import os
 import pandas as pd
 
 
-class MimicCxrMetadata():
+class MimicID:
+	subject_id = ''
+	study_id = ''
+	dicom_id = ''
+
+	def __init__(self, subject_id, study_id, dicom_id):
+		self.subject_id = str(subject_id)
+		self.study_id = str(study_id)
+		self.dicom_id = str(dicom_id)
+
+	def __str__(self):
+		return f"p{self.subject_id}_s{self.study_id}_{self.dicom_id}"
+
+
+class MimicCxrMetadata:
 
 	def __init__(self, mimiccxr_metadata):
-		self.metadata_path = mimiccxr_metadata
-		self.metadata_df = pd.read_csv(self.metadata_path)
+		self.mimiccxr_metadata = pd.read_csv(mimiccxr_metadata)
 
-	def get_sub_columns(self, keys: list):
-		return self.metadata_df[keys]
+	def get_sub_columns(self, columns: list):
+		return self.mimiccxr_metadata[columns]
 
-	def get_sub_rows(self, key: str, value: list):
-		return self.metadata_df[self.metadata_df[key].isin(values)]
+	def get_sub_rows(self, column: str, values: list):
+		return self.mimiccxr_metadata[self.mimiccxr_metadata[column].isin(values)]
 
 	@staticmethod
-	def overlap_by_column(df1, df2, key: str):
-		return df1[df1[key].isin(df2[key])]		
+	def overlap_by_column(metadata1, metadata2, column: str):
+		return metadata1[metadata1[column].isin(metadata2[column])]		
--- image_prep/downsample.py
+++ image_prep/downsample.py
@@ -9,48 +9,97 @@ import torch
 import torchvision
 from torch.utils.data import DataLoader
 import pydicom
+import cv2
 
-from data_utils import MimicCxrMetadata
+from data_utils import MimicID, MimicCxrMetadata
 
 
 @gin.configurable
 class MimicCxrDataset(torchvision.datasets.VisionDataset):
+    """A MIMIC-CXR dataset class that loads dicom images from MIMIC-CXR 
+    given a metadata file and return images in npy
+
+    Args: 
+        mimiccxr_dir (string): Root directory for the MIMIC-CXR dataset.
+        mimiccxr_metadata (string): File path of the entire MIMIC-CXR metadata.
+        dataset_metadata (string): File path of the metadata 
+            that will be used to contstruct this dataset.
+        transform (callable, optional): A function/tranform that takes in an image 
+            and returns a transfprmed version.
+    """
 
     def __init__(self, mimiccxr_dir, mimiccxr_metadata,
-                 dataset_metadata, transform=None):
+                 dataset_metadata, overlap_key='dicom_id', transform=None):
         super(MimicCxrDataset, self).__init__(root=None, transform=transform)
 
         self.mimiccxr_dir = mimiccxr_dir
 
-        mimiccxr_metadata_df = MimicCxrMetadata(mimiccxr_metadata).get_sub_columns(
+        self.mimiccxr_metadata = MimicCxrMetadata(mimiccxr_metadata).get_sub_columns(
             ['subject_id', 'study_id', 'dicom_id'])
-        dataset_metadata_df = MimicCxrMetadata(dataset_metadata).get_sub_columns(
-            ['dicom_id'])
-        metadata_df = MimicCxrMetadata.overlap_by_column(mimiccxr_metadata_df,
-                                                           dataset_metadata_df,
-                                                           'dicom_id')
-        self.metadata_df = metadata_df.reset_index(drop=True)
+       
+        dataset_ids = MimicCxrMetadata(dataset_metadata).get_sub_columns(
+            [overlap_key])
+        self.dataset_metadata = MimicCxrMetadata.overlap_by_column(
+            self.mimiccxr_metadata, dataset_ids, overlap_key).reset_index(drop=True)
         
         self.transform = transform
 
     def __len__(self):
-        return len(self.metadata_df)
+        return len(self.dataset_metadata)
+
+    def select_by_column(self, metadata: str, column: str, values: list):
+        metadata_selected = MimicCxrMetadata(metadata).get_sub_rows(column = column, values=values)
+        self.dataset_metadata = MimicCxrMetadata.overlap_by_column(
+            self.dataset_metadata, metadata_selected, 'dicom_id').reset_index(drop=True)
 
     def __getitem__(self, i):
         subject_id, study_id, dicom_id = \
-            self.metadata_df.loc[i, ['subject_id', 'study_id', 'dicom_id']]
+            self.dataset_metadata.loc[i, ['subject_id', 'study_id', 'dicom_id']]
         dcm_path = os.path.join(
             self.mimiccxr_dir, f'p{subject_id}', f's{study_id}', f'{dicom_id}.dcm')
+        
         if os.path.isfile(dcm_path):
             dcm = pydicom.dcmread(dcm_path)
             img = dcm.pixel_array
             dcm_exists = True
         else:
-            img = np.zeros((256, 256), dtype=np.uint16)
+            img = -1
             dcm_exists = False
+        
         if self.transform is not None:
             img = self.transform(img)
-        return img, dicom_id, dcm_exists
+
+        return img, dcm_exists, str(subject_id), str(study_id), str(dicom_id)
+
+
+def save_png_images(img_size, save_folder, dataset_metadata, overlap_key='dicom_id', view_metadata=None):
+    transform=torchvision.transforms.Compose([
+        torchvision.transforms.Lambda(lambda img: img.astype(np.int32)),
+        # PIL accepts in32, not uint16
+        torchvision.transforms.ToPILImage(),
+        torchvision.transforms.Resize(img_size),
+        torchvision.transforms.Lambda(
+            lambda img: np.array(img).astype(np.int32))
+    ])
+    mimiccxr_dataset = MimicCxrDataset(dataset_metadata=metadata,
+                                       overlap_key=overlap_key,
+                                       transform=transform)
+    print(mimiccxr_dataset.__len__())
+    if view_metadata != None:
+        mimiccxr_dataset.select_by_column(view_metadata, 'view', ['frontal'])
+    print(mimiccxr_dataset.__len__())
+    mimiccxr_loader = DataLoader(mimiccxr_dataset, batch_size=1, shuffle=False,
+                                 num_workers=1, pin_memory=True)
+
+    for i, (img, dcm_exists, subject_id, study_id, dicom_id) in enumerate(mimiccxr_loader):
+        if dcm_exists:
+            img = img.cpu().numpy().astype(np.float)
+            mimic_id = MimicID(subject_id[0], study_id[0], dicom_id[0])
+            png_path = os.path.join(save_folder, f"{mimic_id.__str__()}.png")
+            image = 65535*img[0]/np.amax(img[0])
+            cv2.imwrite(png_path, image.astype(np.uint16))
+            if i%1000==0:
+                print(i)
 
 
 def create_cached_images(img_size, save_folder):
@@ -94,6 +143,10 @@ def create_cached_images(img_size, save_folder):
 
     return imgs, df
 
+
+metadata = '/data/vision/polina/projects/chestxray/'\
+           'work_space_v2/report_processing/edema_labels-12-03-2019/'\
+           'mimic-cxr-sub-img-edema-split-manualtest.csv'
 
 # for im_size in [256]:
 #     start = time.time()
