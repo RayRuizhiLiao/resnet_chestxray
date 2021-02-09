fe7b795198bd313e95c2fb8734cd04f3be12e34d
Ruizhi Liao <ruizhi@csail.mit.edu>
2021-02-09 16:56:53 -0500

    adding script for training

---
+++ train.py
@@ -0,0 +1,56 @@
+'''
+Author: Ruizhi Liao
+
+Main script to run training
+'''
+
+import os
+import argparse
+import logging
+
+from resnet_chestxray.main_utils import ModelManager, build_model
+
+current_dir = os.path.dirname(__file__)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--batch_size', default=256, type=int,
+					help='Mini-batch size')
+
+parser.add_argument('--img_size', default=256, type=int,
+                    help='The size of the input image')
+parser.add_argument('--model_architecture', default='resnet256_6_2_1', type=str,
+                    help='Neural network architecture to be used')
+
+parser.add_argument('--data_dir', type=str,
+					default='/data/vision/polina/scratch/ruizhi/chestxray/data/png_16bit_256/',
+					help='The image data directory')
+parser.add_argument('--dataset_metadata', type=str,
+					default=os.path.join(current_dir, 'data/training.csv'),
+					help='The metadata for the model training ')
+parser.add_argument('--save_dir', type=str,
+					default='/data/vision/polina/scratch/ruizhi/chestxray/experiments/'\
+					'supervised_image/tmp_test_resolution/')
+
+
+def train():
+	args = parser.parse_args()
+
+	print(args)
+
+	if not os.path.exists(args.save_dir):
+		os.makedirs(args.save_dir)
+
+	# Configure the log file
+	log_path = os.path.join(args.save_dir, 'training.log')
+	logging.basicConfig(filename=log_path, level=logging.INFO, filemode='w', 
+						format='%(asctime)s - %(name)s %(message)s', 
+						datefmt='%m-%d %H:%M')
+
+	model_manager = ModelManager(model_name=args.model_architecture, 
+								 img_size=args.img_size)
+	model_manager.train(data_dir=args.data_dir, 
+						dataset_metadata=args.dataset_metadata,
+						batch_size=args.batch_size, save_dir=args.save_dir)
+
+if __file__ == "train.py":
+	train()
