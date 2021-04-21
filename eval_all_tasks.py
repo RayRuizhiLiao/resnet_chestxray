9e37e2484f172370247916f3487a6a688168f722
Ruizhi Liao <ruizhi@csail.mit.edu>
2021-04-20 20:28:07 -0400

    updating

--- eval_all_tasks.py
+++ eval_all_tasks.py
@@ -1,7 +1,7 @@
 '''
 Author: Ruizhi Liao
 
-Main script to run evaluation
+Main script to run evaluation for all tasks
 '''
 
 import os
@@ -42,24 +42,16 @@ parser.add_argument('--checkpoint_name', type=str,
 					default='pytorch_model_epoch300.bin')
 
 
-def eval(all_epochs=-1):
-	args = parser.parse_args()
-
+def eval(args, device, all_epochs=-1):
 	print(args)
-
-	'''
-	Check cuda
-	'''
-	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-	assert torch.cuda.is_available(), "No GPU/CUDA is detected!"
 
 	'''
 	Create a sub-directory under save_dir 
 	based on the label key
 	'''
-	args.save_dir = os.path.join(args.save_dir, args.model_architecture+'_'+args.label_key)
+	save_dir = os.path.join(args.save_dir, args.model_architecture+'_'+args.label_key)
 	
-	checkpoint_path = os.path.join(args.save_dir, args.checkpoint_name)
+	checkpoint_path = os.path.join(save_dir, args.checkpoint_name)
 
 	model_manager = ModelManager(model_name=args.model_architecture, 
 								 img_size=args.img_size,
@@ -97,4 +89,49 @@ def eval(all_epochs=-1):
 		with open(results_path, 'w') as fp:
 			json.dump(eval_results_all, fp)
 
-eval(all_epochs=-1)
+		return eval_results_all
+
+	return eval_results
+
+def eval_all_tasks(all_epochs=-1):
+	args = parser.parse_args()
+	print(args)
+
+	'''
+	Check cuda
+	'''
+	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+	assert torch.cuda.is_available(), "No GPU/CUDA is detected!"
+
+	eval_results_all_tasks = {}
+	'''
+	Chexpert tasks
+	'''
+	labels = ['Atelectasis','Cardiomegaly','Consolidation','Edema',\
+			  'LungOpacity','PleuralEffusion',\
+			  'Pneumonia','Pneumothorax','SupportDevices']
+	args.dataset_metadata = os.path.join(current_dir, 'data/test_chexpert.csv')
+	args.output_channels = 1
+
+	for label_key in labels:
+		args.label_key = label_key
+		eval_results_all_tasks[args.label_key] = eval(args, device, all_epochs=all_epochs)
+	
+	'''
+	Edema severity task
+	'''
+	# args.dataset_metadata = os.path.join(current_dir, 'data/test_edema.csv')
+	# args.output_channels = 4
+
+	# args.label_key = 'edema_severity'
+	# eval_results_all_tasks[args.label_key] = eval(args, device, all_epochs=all_epochs)
+
+	print(eval_results_all_tasks)
+
+	for label_key in labels:
+		print(eval_results_all_tasks[label_key]['aucs'][0])
+	# print(eval_results_all_tasks['edema_severity']['ordinal_aucs'][0])
+	# print(eval_results_all_tasks['edema_severity']['ordinal_aucs'][1])
+	# print(eval_results_all_tasks['edema_severity']['ordinal_aucs'][2])
+
+eval_all_tasks()
